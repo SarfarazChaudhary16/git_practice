f57213c71d7bd499da445b89648586b3baf71b9b
SarfarazChaudhary16 <sarfaraz16chaudhary@gmail.com>
2024-07-30 16:15:26 +0530

    first commit

---
+++ if-else.java
@@ -0,0 +1,16 @@
+
+class if_else{
+    public static void main(String[] args) {
+
+        int i= 0;
+        if(i>0){
+            System.out.println("The number is positive");
+        }
+        else if(i<0){
+            System.out.println("The number is negative");
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+    }
+}
